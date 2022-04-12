a0c031df6fd6a065897189c3047d36e0d204acf7
Jokerinya <sakacibrahim@gmail.com>
2022-04-12 06:56:03 +0300

    files seperated

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 // 3 party
 const express = require('express');
+const ejs = require('ejs');
 
 require('dotenv').config();
 
 const app = express();
+// Template engine
+app.set('view engine', 'ejs');
+// middlewares
+app.use(express.static(__dirname + '/public'));
+
+//Routes
 app.get('/', async (req, res) => {
-  res.status(200).send('Index page');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+app.get('/about', async (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 const port = process.env.PORT || 3000;
