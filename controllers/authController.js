f20fb2750639d0c5897b893b142d187f1c31a8d9
Jokerinya <sakacibrahim@gmail.com>
2022-04-18 20:08:20 +0300

    teacher course adding modal added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const Category = require('../models/Category');
 
 exports.createUser = async (req, res) => {
   try {
@@ -48,9 +49,11 @@ exports.logoutUser = async (req, res) => {
 exports.getDashboardPage = async (req, res) => {
   try {
     const user = await User.findById(req.session.userID);
+    const categories = await Category.find();
     res.status(200).render('dashboard', {
       page_name: 'dashboard',
       user,
+      categories,
     });
   } catch (error) {
     res.status(400).json({
