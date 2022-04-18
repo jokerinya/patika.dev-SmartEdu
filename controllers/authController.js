1f3c868cadfe75c6a534233fef638d2b892bf6f6
Jokerinya <sakacibrahim@gmail.com>
2022-04-18 19:32:03 +0300

    user roles has been created

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,7 @@ const User = require('../models/User');
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      user,
-    });
+    res.status(201).redirect('/login');
   } catch (error) {
     res.status(400).json({
       status: 'error',
